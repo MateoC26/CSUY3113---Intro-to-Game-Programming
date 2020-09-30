2028810cc99429e333e7230ea4d5dc4eec1b2e31
Mateo <mc7212@nyu.edu>
2020-09-29 23:51:33 -0400

    Added features to P2

--- P2/main.cpp
+++ P2/main.cpp
@@ -25,12 +25,14 @@
 #include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
+#include "vector"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
+bool gameStarted = false;
 bool gameOver = false;
 
 ShaderProgram program;
@@ -45,8 +47,8 @@ glm::vec3 rightPaddle_movement = glm::vec3(0, 0, 0);
 glm::vec3 ball_position = glm::vec3(0, 0, 0);
 glm::vec3 ball_movement = glm::vec3(0, 0, 0);
 
-float ball_speed = 3.0f;
-float paddle_speed = 4.0f;
+float ball_speed = 6.0f;
+float paddle_speed = 10.0f;
 
 float ball_radius = 0.15f;
 float paddle_height = 1.0f;
@@ -57,6 +59,7 @@ int rightPaddle_score = 0;
 
 GLuint paddleTextureID;
 GLuint ballTextureID;
+GLuint fontTextureID;
 
 GLuint LoadTexture(const char* filePath)
 {
@@ -107,14 +110,14 @@ void Initialize()
 
 	glUseProgram(program.programID);
 
-	glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
+	glClearColor(1.0f, 0.75f, 0.5f, 1.0f);
 	glEnable(GL_BLEND);
 
-	// Good setting for transparency
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	paddleTextureID = LoadTexture("paddle.png");
 	ballTextureID = LoadTexture("ball.png");
+	fontTextureID = LoadTexture("font.png");
 }
 
 void ProcessInput()
@@ -135,11 +138,50 @@ void ProcessInput()
 		case SDL_KEYDOWN:
 			switch (event.key.keysym.sym)
 			{
+		    //Sets ball into movement
 			case SDLK_SPACE:
+				//Only occurs if the ball is in the middle and the game is not over
 				if (ball_movement.x == 0 && ball_movement.y == 0 && !gameOver)
 				{
-					ball_movement.x = 1.0f;
-					ball_movement.y = 1.0f;
+					//Sets gameStarted to true and randomizes ball movement
+					gameStarted = true;
+					int rand_x = rand() % 100;
+					int rand_y = rand() % 100;
+
+					if (rand_x < 50)
+					{
+						ball_movement.x = 1.0f;
+					}
+					else
+					{
+						ball_movement.x = -1.0f;
+					}
+
+					if (rand_y < 50)
+					{
+						ball_movement.y = 1.0f;
+					}
+					else
+					{
+						ball_movement.y = -1.0f;
+					}
+				}
+				break;
+			//Restarts game
+			case SDLK_r:
+				if (gameOver)
+				{
+					gameOver = 0;
+					gameStarted = 0;
+					leftPaddle_score = 0;
+					rightPaddle_score = 0;
+				}
+				break;
+			//Closes game
+			case SDLK_ESCAPE:
+				if (gameOver)
+				{
+					gameIsRunning = false;
 				}
 				break;
 			}
@@ -149,8 +191,9 @@ void ProcessInput()
 
 	const Uint8* keys = SDL_GetKeyboardState(NULL);
 
-	if (!gameOver)
+	if (!gameOver && gameStarted)
 	{
+		//Controls left paddle, W is up and S is down
 		if (keys[SDL_SCANCODE_W] && leftPaddle_position.y + paddle_height / 2 < 3.75f)
 		{
 			leftPaddle_movement.y = 1.0f;
@@ -160,6 +203,7 @@ void ProcessInput()
 			leftPaddle_movement.y = -1.0f;
 		}
 
+		//Controls right paddle, ARROW UP is up and ARROW DOWN is down
 		if (keys[SDL_SCANCODE_UP] && rightPaddle_position.y + paddle_height / 2 < 3.75f)
 		{
 			rightPaddle_movement.y = 1.0f;
@@ -170,18 +214,18 @@ void ProcessInput()
 		}
 	}
 
-	// Normalize vector if it goes over speed
+	// Normalize vector if it goes over speed for both paddles
 	if (glm::length(leftPaddle_movement) > 1.0f)
 	{
 		leftPaddle_movement = glm::normalize(leftPaddle_movement);
 	}
-
 	if (glm::length(rightPaddle_movement) > 1.0f)
 	{
 		rightPaddle_movement = glm::normalize(rightPaddle_movement);
 	}
 }
 
+//Resets ball to center of the screen and stops movement
 void resetBall()
 {
 	ball_position.x = 0;
@@ -191,6 +235,7 @@ void resetBall()
 	ball_movement.y = 0;
 }
 
+//Resets paddles and ball
 void reset()
 {
 	resetBall();
@@ -215,6 +260,8 @@ void Update()
 
 	ballMatrix = glm::mat4(1.0f);
 	ballMatrix = glm::translate(ballMatrix, ball_position);
+
+	//Incrementing score if the ball hits side wall
 	if (ball_position.x + ball_radius > 5.0f)
 	{
 		resetBall();
@@ -226,6 +273,7 @@ void Update()
 		rightPaddle_score++;
 	}
 
+	//Bouncing off top and bottom walls
 	if (ball_position.y + ball_radius > 3.75f)
 	{
 		ball_movement.y = -1.0f;
@@ -248,18 +296,18 @@ void Update()
 	rightPaddleMatrix = glm::translate(rightPaddleMatrix, rightPaddle_position);
 
 	//Collision
-	float xdist = fabs(ball_position.x - leftPaddle_position.x) - ((paddle_width +	ball_radius) / 2.0f);
-	float ydist = fabs(ball_position.y - leftPaddle_position.y) - ((paddle_height + ball_radius) / 2.0f);
+	float xdist_left = fabs(ball_position.x - leftPaddle_position.x) - ((paddle_width +	ball_radius) / 2.0f);
+	float ydist_left = fabs(ball_position.y - leftPaddle_position.y) - ((paddle_height + ball_radius) / 2.0f);
 
-	float xdist1 = fabs(ball_position.x - rightPaddle_position.x) - ((paddle_width + ball_radius) / 2.0f);
-	float ydist1 = fabs(ball_position.y - rightPaddle_position.y) - ((paddle_height + ball_radius) / 2.0f);
+	float xdist_right = fabs(ball_position.x - rightPaddle_position.x) - ((paddle_width + ball_radius) / 2.0f);
+	float ydist_right = fabs(ball_position.y - rightPaddle_position.y) - ((paddle_height + ball_radius) / 2.0f);
 
-	if (xdist < 0 && ydist < 0)
+	if (xdist_left < 0 && ydist_left < 0)
 	{
 		ball_movement.x = 1.0f;
 	}
 
-	if (xdist1 < 0 && ydist1 < 0)
+	if (xdist_right < 0 && ydist_right < 0)
 	{
 		ball_movement.x = -1.0f;
 	}
@@ -298,6 +346,61 @@ void drawBall()
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
+void DrawText(ShaderProgram* program, GLuint fontTextureID, std::string text,
+	          float size, float spacing, glm::vec3 position)
+{
+	float width = 1.0f / 16.0f;
+	float height = 1.0f / 16.0f;
+
+	std::vector<float> vertices;
+	std::vector<float> texCoords;
+
+	for (int i = 0; i < text.size(); i++)
+	{
+		int index = (int)text[i];
+		float offset = (size + spacing) * i;
+
+		float u = (float)(index % 16) / 16.0f;
+		float v = (float)(index / 16) / 16.0f;
+
+		vertices.insert(vertices.end(), {
+			offset + (-0.5f * size), 0.5f * size,
+			offset + (-0.5f * size), -0.5f * size,
+			offset + (0.5f * size), 0.5f * size,
+			offset + (0.5f * size), -0.5f * size,
+			offset + (0.5f * size), 0.5f * size,
+			offset + (-0.5f * size), -0.5f * size
+			});
+
+		texCoords.insert(texCoords.end(), {
+			u, v,
+			u, v + height,
+			u + width, v,
+			u + width, v + height,
+			u + width, v,
+			u, v + height
+			});
+	}
+
+	glm::mat4 modelMatrix = glm::mat4(1.0f);
+	modelMatrix = glm::translate(modelMatrix, position);
+	program->SetModelMatrix(modelMatrix);
+
+	glUseProgram(program->programID);
+
+	glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices.data());
+	glEnableVertexAttribArray(program->positionAttribute);
+
+	glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords.data());
+	glEnableVertexAttribArray(program->texCoordAttribute);
+
+	glBindTexture(GL_TEXTURE_2D, fontTextureID);
+	glDrawArrays(GL_TRIANGLES, 0, (int)(text.size() * 6));
+
+	glDisableVertexAttribArray(program->positionAttribute);
+	glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
 void Render()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -313,6 +416,45 @@ void Render()
 	drawLeftPaddle();
 	drawRightPaddle();
 	drawBall();
+
+	//Checks if game has started and displays respective text
+	if (!gameStarted)
+	{
+		DrawText(&program, fontTextureID, "First Player To 5 Points Wins", 0.5f, -0.25f,
+			glm::vec3(-3.5f, 3.0f, 0));
+
+		DrawText(&program, fontTextureID, "Press SPACE To Start", 0.5f, -0.25f, 
+			glm::vec3(-2.3f, 2.5f, 0));
+	}
+	else
+	{
+		DrawText(&program, fontTextureID, std::to_string(leftPaddle_score),
+			1.0f, -0.25f, glm::vec3(-2.0f, 3.0f, 0));
+
+		DrawText(&program, fontTextureID, std::to_string(rightPaddle_score),
+			1.0f, -0.25f, glm::vec3(2.0f, 3.0f, 0));
+	}
+
+	//Displays winner and options to close or play again
+	if (gameOver)
+	{
+		if (leftPaddle_score == 5)
+		{
+			DrawText(&program, fontTextureID, "Player 1 Wins!", 0.5f, -0.25f, 
+				glm::vec3(-1.6f, 1.0f, 0));
+		}
+		else
+		{
+			DrawText(&program, fontTextureID, "Player 2 Wins!", 0.5f, -0.25f,
+				glm::vec3(-1.6f, 1.0f, 0));
+		}
+
+		DrawText(&program, fontTextureID, "Press R To Play Again", 0.5f, -0.25f,
+			glm::vec3(-2.5f, -1.0f, 0));
+
+		DrawText(&program, fontTextureID, "Press ESC To Close", 0.5f, -0.25f,
+			glm::vec3(-2.1f, -2.0f, 0));
+	}
 
 	glDisableVertexAttribArray(program.positionAttribute);
 	glDisableVertexAttribArray(program.texCoordAttribute);
